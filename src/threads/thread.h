b020f3ed4d6885df883709962b41af65c2d871d5
ash15764 <136904070+ash15764@users.noreply.github.com>
2025-05-14 07:35:44 +0300

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -90,6 +90,7 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all treads list. */
+    struct file *fd_table[128];
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
